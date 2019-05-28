61d3513f225cb1439e0d6b20fa16676eb8dd857e
romanets <virucman@gmail.com>
2019-05-28 22:04:14 +0300

    8комит. Добавлен роут articles и добавлен ArticlesController который этот роут обрабатывает и выводит одну статью во View

---
+++ src/MyProject/Controllers/ArticlesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MyProject\Controllers;
+
+use MyProject\Services\Db;
+use MyProject\View\View;
+
+
+class ArticlesController
+{
+
+    private $view;
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../../templates');
+        $this->db = new Db();
+    }
+
+    public function view(int $articleId)
+    {
+        $result = $this->db->query(
+            'SELECT * FROM `articles` WHERE id = :id;',
+            [':id' => $articleId]
+        );
+    
+        if ($result === []) {
+            // Здесь обрабатываем ошибку
+            return;
+        }
+    
+        $this->view->renderHtml('articles/view.php', ['article' => $result[0]]);
+    }
+
+}
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
