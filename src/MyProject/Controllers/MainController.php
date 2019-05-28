3a16878bfeeb5c02309f9e28e3c8e65974af3e0e
romanets <virucman@gmail.com>
2019-05-28 20:23:11 +0300

    2комит. +стили, tamplate с 2 статьями из контт-ра

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -4,13 +4,16 @@ namespace MyProject\Controllers;
 
 class MainController
 {
+
+
     public function main()
     {
-        echo 'Главная страница';
+            $articles = [
+            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
+        ];
+        include __DIR__ . '/../../../templates/main/main.php';
     }
 
-    public function sayHello(string $name)
-    {
-        echo 'Привет, ' . $name;
-    }
+  
 }
