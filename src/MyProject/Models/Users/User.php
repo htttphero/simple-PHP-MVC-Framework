eafd124167810bd439bd6952e3bcf9b5c77a903a
romanets <virucman@gmail.com>
2019-05-29 13:05:11 +0300

    14комит. Добавили модель User которую отнаследовали от базового класса ActiveRecord

---
+++ src/MyProject/Models/Users/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MyProject\Models\Users;
+
+use MyProject\Models\ActiveRecordEntity;
+
+class User extends ActiveRecordEntity
+{
+  
+    protected $nickname;
+
+    
+    protected $email;
+
+ 
+    protected $isConfirmed;
+
+ 
+    protected $role;
+
+  
+    protected $passwordHash;
+
+ 
+    protected $authToken;
+
+     
+    protected $createdAt;
+
+   
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    protected static function getTableName(): string
+    {
+        return 'users';
+    }
+ 
+}
